4d1fff6a51a64cd38edefc01fc0a1d43eeea817c
locbuig <locbuig@gmail.com>
2019-12-30 17:36:54 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [compSel, setCompSel] = useState('');
+  const [playerSel, setPlayerSel] = useState('');
+  const [result, setResult] = useState('');
+  const [playerWin, setPlayerWin] = useState(0);
+  const [compWin, setCompWin] = useState(0);
+  const [currRound, setCurrRound] = useState(0);
+
+  let numOfRound = 5;
+
+  const choices = ['rock', 'paper', 'scissor']
+
+  function computerPlay() {
+    return choices[Math.floor(Math.random()*choices.length)]
+  }
+
+  function playRound(playerSelection, compSelection) {
+    console.log(playerSelection, compSelection)
+    const x = choices.indexOf(playerSelection);
+    const y = choices.indexOf(compSelection);
+    if (x === y) {
+      setCurrRound(currRound + 1);
+      return "Tie";
+    }
+
+    if (x-y === 1 || x-y === -2) {
+      setPlayerWin(playerWin + 1);
+      setCurrRound(currRound + 1);
+      return "Player wins this round with " + playerSelection;
+    } else if (y-x === 1 || y-x === -2) {
+      setCompWin(compWin + 1);
+      setCurrRound(currRound + 1);
+      return "Comp wins this round with " + compSelection;
+    }
+  }
+
+  function handleChange(event) {
+    setPlayerSel(event.target.value)
+  }
+
+  function handleSubmit(e) {
+    const comp = computerPlay()
+    setCompSel(comp)
+    setResult(playRound(playerSel, comp))
+    e.preventDefault()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label>Player choose:</label>
+        <div className="radio">
+          <label>
+            <input type="radio" value="rock" checked={playerSel === 'rock'} onChange={e => handleChange(e)}/>
+            Rock
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio" value="paper" checked={playerSel === 'paper'} onChange={e => handleChange(e)}/>
+            Paper
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio" value="scissor" checked={playerSel === 'scissor'} onChange={e => handleChange(e)}/>
+            Scissor
+          </label>
+        </div>
+        {currRound !== numOfRound && <input type="submit" value="Submit" />}
+      </form>
+      {result && result !== '' && <h4>Comp choose {compSel}</h4>}
+      <h4>{result}</h4>
+      <h4>Current Point: Player {playerWin} - Comp {compWin}</h4>
+      {currRound === numOfRound && <h4>Final Result: {playerWin === compWin ? 'Tie' : playerWin > compWin ? 'Player Win' : 'Comp Win'}</h4>}
     </div>
   );
 }
